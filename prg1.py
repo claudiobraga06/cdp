1f29028f0c3bdd5997437af3491c2e58077ba1bd
claudiobraga06 <claudiobraga06@gmail.com>
2025-09-19 15:22:58 -0300

    Add confirmation print statement in prg1.py
    Added a print statement to confirm successful execution.

--- prg1.py
+++ prg1.py
@@ -2,3 +2,5 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("CreateTable").enableHiveSupport().getOrCreate()  
 spark.sql("use u00389026727") 
 spark.sql("create table tbl1 using iceberg as select * from fl_parq where origin = 'EWR' limit 10")
+print("tudo funcionou corretamente")
+
